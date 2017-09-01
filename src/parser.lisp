6f13575a95745f2aa16a4437a973764e9bfaadd0
odanoburu <bcclaro@gmail.com>
2017-09-01 11:57:33 -0300

    add xml constructor

--- src/parser.lisp
+++ src/parser.lisp
@@ -8,8 +8,14 @@
 ; https://github.com/Ramarren/cl-parser-combinators/blob/master/doc/parser-combinators.org
 
 (defpackage :oab-parser
-  (:use :cl :parser-combinators :parser-combinators-cl-ppcre :alexandria)
-  (:export #:parse-oab #:parse-oab-file))
+  (:use :cl
+        :parser-combinators
+        :parser-combinators-cl-ppcre
+        :alexandria)
+  (:export #:parse-oab
+           #:parse-oab-file
+           #:dir-to-xml
+           #:exam-to-xml))
 
 (in-package :oab-parser)
 
@@ -26,7 +32,8 @@
   (sepby? (paragraph?) #\Newline))
 
 (defun sep-options? ()
-  (seq-list? (between? #\Newline 1 2) "OPTIONS" (between? #\Newline 1 2)))
+  (seq-list? (between? #\Newline 1 2) "OPTIONS"
+             (between? #\Newline 1 2)))
 
 (defun option? ()
   (named-seq? #\Newline
@@ -66,14 +73,61 @@
 	(declare (ignore a))
 	(cl-ppcre:do-scans (s e rs re sc2 ops)
 	  (push (list (subseq ops (aref rs 0) (aref re 0))
-		      (if (aref rs 1) (subseq ops (aref rs 1) (aref re 1)))
+		      (if (aref rs 1) (subseq ops (aref rs 1)
+                                              (aref re 1)))
 		      (subseq ops (aref rs 2) (aref re 2)))
 		res))
 	(list (aref m 0) (aref m 1) res)))))
 
 
 (defun parse-oab-file (filename &key (fn-parsing #'naive-parser))
-  (let ((questions (cdr (cl-ppcre:split "---\\s" (read-file-into-string filename)))))
+  (let ((questions (cdr
+                    (cl-ppcre:split "---\\s"
+                                    (read-file-into-string filename)))))
     (mapcar fn-parsing questions)))
 
+;;
+;; XML
 
+(defun true-or-false (boolean)
+  (if boolean
+      "true"
+      "false"))
+
+(defun item-to-tree (item)
+  (destructuring-bind (i-letter i-correct? i-text) item
+    (list "item" (list (list "letter" i-letter)
+                       (list "correct" (true-or-false i-correct?)))
+          i-text)))
+
+(defun question-to-tree (question)
+  (destructuring-bind (q-number q-enum items) question
+    (list "question" (list (list "number" q-number))
+          (list "statement" nil q-enum)
+          (append (list "items" nil)
+                  (reverse (mapcar #'item-to-tree items))))))
+
+(defun questions-to-tree (questions year)
+  (list "OAB-exam" (list (list "year" year) (list "edition" ""))
+        (append (list "questions" nil)
+                (mapcar #'question-to-tree questions))))
+
+(defun tree-to-xml (tree path)
+  (with-open-file (out path :direction :output
+                       :element-type '(unsigned-byte 8))
+    (cxml-xmls:map-node (cxml:make-octet-stream-sink out)
+                        tree :include-namespace-uri nil)))
+
+(defun exam-to-xml (path)
+  (let* ((questions (oab-parser:parse-oab-file path))
+         (year (subseq (file-namestring path) 0 4))
+         (tree (questions-to-tree questions year))
+         (xml-path (make-pathname :type "xml" :defaults path)))
+    (tree-to-xml tree xml-path)))
+
+(defun dir-to-xml (dir-path)
+  "use path/to/txt/files/*.txt to get all *.txt files in the
+directory"
+  (let ((file-paths (directory dir-path)))
+    (mapcar #'exam-to-xml file-paths))
+  nil)
