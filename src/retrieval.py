aef21b2afb0384d5640991b099f302a4c9c3eb9a
odanoburu <bcclaro@gmail.com>
2017-09-10 11:38:48 -0300

    add artigos preprocessing and tf-idf calculation

--- src/retrieval.py
+++ src/retrieval.py
@@ -48,12 +48,83 @@ def articles_in_tree(tree_root, namespace=nsm):
 """
 # examples
 
-oab = parse_xml('~/git/oab-exams/src/2010-01.xml')
+oab = parse_xml('/home/bruno/git/oab-exams/src/2010-01.xml')
 questions = parsed_questions_in_tree(oab)
 next(questions)
 
-lei = parse_xml('~/git/oab-exams/lexml/lei-8906.xml')
-artigos =  articles_in_tree(a)
+lei = parse_xml('/home/bruno/git/oab-exams/lexml/lei-8906.xml')
+artigos =  articles_in_tree(lei)
 next(artigos)
+
+"""
+
+#
+## tf-idf
+import nltk
+import numpy
+import collections
+nltk.download('punkt')
+  
+def is_number(token):
+    try:
+        float(token.replace(',', '.').replace('º', ''))
+    except ValueError:
+        return False
+    return True
+
+def is_punctuation(token):
+    if token in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`´{|}~§–':
+        return True
+
+def is_stopword(token, language='portuguese'):
+    if token in nltk.corpus.stopwords.words(language):
+        return True
+
+def is_useful(token, rm_stopwords):
+    token = token.strip()
+    if is_number(token) or is_punctuation(token) or (rm_stopwords and is_stopword(token)):
+        return False
+    else:
+        return True
+
+def preprocess_text(text, rm_stopwords):
+    assert bool(rm_stopwords)
+    return [token.lower().strip() for token in nltk.tokenize.word_tokenize(text) if is_useful(token, rm_stopwords)]
+
+class ArtigosCollection(nltk.TextCollection):
+    def __init__(self, source, rm_stopwords=False):
+        self.ids = {artigo[0]:ix for ix, artigo in enumerate(source)}
+        nltk.TextCollection.__init__(self, list(map(lambda x: preprocess_text(x[1], rm_stopwords), source)))
+        self.index_dict = {key:ix for ix, key in enumerate(self.vocab().keys())}
+        self.vocab_size = len(self.vocab().keys())
+        self.tfidf_vectors = [self.tfidf_vectorize(text) for text in self._texts]
+
+    def tf_tokens(self, tokens):
+        count = collections.Counter(tokens)
+        length = len(tokens)
+        return list(map(lambda x: count[x]/length, tokens))
+
+    def tfidf_vectorize(self, text):
+        tfidf_vector = numpy.zeros(self.vocab_size)
+        tf_vector = self.tf_tokens(text)
+        for ix, token in enumerate(text):
+            idf = self.idf(token)
+            if idf == 0:
+                continue
+            tfidf_vector[self.index_dict[token]] = tf_vector[ix] * idf
+        return tfidf_vector
+
+def cosine_similarity(vec1, vec2):
+    denominator = numpy.linalg.norm(vec1) * numpy.linalg.norm(vec2)
+    if denominator == 0:
+        return 0
+    else:
+        return numpy.dot(vec1, vec2) / denominator
+
+"""
+examples:
+
+arts = preprocess_artigos(artigos, rm_stopwords=True)
+artcol = ArtigosCollection(arts)
 
 """
