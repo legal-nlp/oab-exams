805b5f39990af9ee8929d9722feead54a8343e80
Pedro Delfino <p.delfino01@gmail.com>
2018-02-10 10:12:43 -0200

    improve scritp clean_data and insert_answer

--- simulated/raw/insert_answer_simulated.py
+++ simulated/raw/insert_answer_simulated.py
@@ -26,11 +26,28 @@ if as_len != 70:
     
     exit("did not get all the answers from the answer-sheet, exit the program")
 
-# print (answer_list)
-
 iter_num = -1
 
 original_file = input("insert the name of the raw file, still missing the answer-sheet: ")
+
+def convert_to_string(lista):
+    
+    new_list = []
+
+    for i in lista:
+
+        new_list.append(str(i))
+
+    return new_list 
+
+zero_to_nine = list(range(0,10))
+
+zero_to_nine = convert_to_string(zero_to_nine)
+
+ten_to_eighty = list(range(10,81))
+
+ten_to_eighty = convert_to_string(ten_to_eighty)
+
 
 with open(original_file, 'r') as input_file, open('output_file.txt', 'w') as output_file:
     
@@ -55,11 +72,30 @@ with open(original_file, 'r') as input_file, open('output_file.txt', 'w') as out
         elif line[:7]=="OPTIONS":
         
             iter_num += 1
+            
+            output_file.write(line)
+        
+        elif (line[0] in zero_to_nine) and (line[1]=="."):
+        
+            deleted_num_point = line[0]+line[1]
+
+            line = line.replace(deleted_num_point,"")
+
+            output_file.write(line)
+
+
+        elif (line[:2] in ten_to_eighty) and (line[2]=="."):
+
+            deleted_num_point = line[0]+line[1]+line[2]
+
+            line = line.replace(deleted_num_point,"")
+
             output_file.write(line)
 
         else:
 
             # print(line)
             output_file.write(line)
+
 
 
