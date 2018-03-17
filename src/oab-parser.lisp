46c0b97aa1a988b25dedfd76965d6ebd6559d1e6
Alexandre Rademaker <arademaker@gmail.com>
2018-03-17 17:56:28 -0300

    correcoes de errors achadas pelo parser

--- src/oab-parser.lisp
+++ src/oab-parser.lisp
@@ -4,24 +4,58 @@
 (ql:quickload :cxml)
 (ql:quickload :yason)
 
-
 (defpackage :oab-parser
   (:use :cl :alexandria))
 
 (in-package :oab-parser)
 
-(defun naive-parser (inputs)
+(defclass question ()
+  ((filename :accessor question-filename :initarg :filename)
+   (number   :accessor question-number)
+   (valid    :accessor question-valid)
+   (enum     :accessor question-enum)
+   (areas    :accessor question-areas    :initform nil)
+   (options  :accessor question-options  :initform nil)))
+
+(defclass option ()
+  ((letter  :accessor option-letter  :initarg :letter)
+   (correct :accessor option-correct :initarg :correct)
+   (text    :accessor option-text    :initarg :text :initform nil)))
+
+
+(defun join-lines (lines)
+  (string-trim '(#\Space) (format nil "~{~a~^ ~}" lines)))
+
+(defun enum-pp (lines &optional (paragraph nil) (paragraphs nil))
+  (cond ((null lines)
+	 (format nil "~{~a~^~%~% ~}" (reverse paragraphs)))
+	((equal "" (car lines))
+	 (enum-pp (cdr lines)
+		  nil
+		  (if paragraph
+		      (cons (join-lines (reverse paragraph)) paragraphs)
+		      paragraphs)))
+	(t
+	 (enum-pp (cdr lines)
+		  (cons (car lines) paragraph)
+		  paragraphs))))
+
+
+(defun naive-parser (inputs filename)
   (macrolet ((flush-line ()
 	       `(setq line (let ((l (car lines)))
 			     (if l (string-trim '(#\Space) l) l))
 		      lines (cdr lines))))
-    (prog (line (lines inputs) head enum item (items nil))
+    (prog (line (lines inputs) enum (item nil) (items nil)
+	   (q (make-instance 'question :filename filename)))
+       
      label-1
      (flush-line)
      (multiple-value-bind (a b)
 	 (cl-ppcre:scan-to-strings "^ENUM[ ]*(NULL)?[ ]*Questão ([0-9]+)" line)
        (cond 
-	 (a (push b head)
+	 (a (setf (question-number q) (aref b 1)
+		  (question-valid q)  (if (equal "NULL" (aref b 0)) nil t))
 	    (go label-2))
 	 ((null a) (error "invalid question"))))
 
@@ -32,7 +66,7 @@
        (cond
 	 ((equal "" line)
 	  (go label-2))
-	 (a (push b head)
+	 (a (push (aref b 0) (question-areas q))
 	    (go label-2))
 	 ((null a)
 	  (push line enum)
@@ -47,35 +81,49 @@
 	 ((or (null a) (equal "" line))
 	  (push line enum)
 	  (go label-3))
-	 (a (go label-4))))
+	 (a (setf (question-enum q) (enum-pp (reverse enum)))
+	    (go label-4))))
 
      label-4
      (flush-line)
      (multiple-value-bind (a b)
-	 (cl-ppcre:scan-to-strings "^([A-D])(:CORRECT)?\\)" line)
+	 (cl-ppcre:scan-to-strings "^([A-D])(:CORRECT)?\\)(.*)$" line)
        (cond
 	 ((null line)
-	  (if item (push (reverse item) items))
+	  (if item
+	      (progn (setf (option-text item)
+			   (join-lines (reverse (option-text item))))
+		     (push item items)))
 	  (go label-5))
-	 (a (push (list b line) item)
+	 (a (setf item (make-instance 'option
+				      :letter  (aref b 0)
+				      :correct (aref b 1)))
+	    (push (aref b 2) (option-text item))
 	    (go label-4))
 	 ((and (null a) (not (equal "" line)))
-	  (push line item)
+	  (push line (option-text item))
 	  (go label-4))
 	 ((equal "" line)
-	  (if item (push (reverse item) items))
+	  (if item
+	      (progn (setf (option-text item)
+			   (join-lines (reverse (option-text item))))
+		     (push item items)))
 	  (setf item nil)
 	  (go label-4))))
 
      label-5
-     (return (list (reverse head) (reverse enum) (reverse items))))))
+     (setf (question-options q) (reverse items))
+     (assert (or (member ":CORRECT" (mapcar #'option-correct items) :test #'equal)
+		 (not (question-valid q))))
+     (return q))))
 
 
-(defun parse-oab-file (filename &key (fn-parsing #'naive-parser))
-  (let ((questions (cdr (cl-ppcre:split "---[ ]*\\n" (read-file-into-string filename)))))
+(defun parse-oab-file (filename &key (parser #'naive-parser))
+  (let ((questions (cdr (cl-ppcre:split "---[ \\t]*\\n" (read-file-into-string filename)))))
     (mapcar (lambda (txt)
-	      (list filename
-		    (funcall fn-parsing (cl-ppcre:split "\\n" txt))))
+	      (funcall parser
+		       (cl-ppcre:split "\\n" txt)
+		       (pathname-name filename)))
 	    questions)))
 
 
