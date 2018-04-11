aff333db31de18a3b8b53a1bd1ec11454def3934
Fabricio Chalub <fchalub@br.ibm.com>
2018-04-11 07:43:43 -0300

    rearranged files a bit

--- experiments/PMI/report.py
+++ experiments/PMI/report.py
@@ -2,13 +2,19 @@
 
 import json, sys, statistics
 
+def calculate_mean_pmi(questions):
+    for q in questions:
+        for o in q['options']:
+            o['pmi'] = statistics.mean(o['pmi'])
+
 for fn in sys.argv[1:]:
     questions = []
     with open(fn,'r') as f:
         questions.extend(json.load(f))
-    for q in questions:
-        for o in q['options']:
-            o['pmi'] = statistics.mean(o['pmi'])
+
+    calculate_mean_pmi(questions)
+
+    ## what happens if there are collisions?
     for q in questions:
         max_pmi = q['options'][0]['pmi']
         chosen_answer = None
